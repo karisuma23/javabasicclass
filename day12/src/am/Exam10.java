ede63f31c6198eb64c2b83094226518b9b29a258
karisuma23 <96929176+karisuma23@users.noreply.github.com>
2022-03-23 16:52:51 +0900

    1

---
+++ day12/src/am/Exam10.java
@@ -0,0 +1,15 @@
+package am;
+
+public class Exam10 {
+
+	public static void main(String[] args) {
+		// 실행 중인 쓰레드의 갯수 확인 
+		Thread curThread = Thread.currentThread();
+		System.out.println("현재 쓰레드의 이름 = " + curThread.getName());
+		System.out.println("현재 동작중인 개수 = " + curThread.activeCount());
+		
+		
+
+	}
+
+}
